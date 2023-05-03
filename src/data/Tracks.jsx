f5f04c9c0f7d9165a0738b5965ad9c5934c2f820
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-03 01:02:12 +0100

    Add data

---
+++ src/data/Tracks.jsx
@@ -0,0 +1,11 @@
+import beautiful from './a_beautiful_day.mp3';
+import trinix from './trinix.jpeg';
+
+export const tracks = [
+  {
+    title: 'Trinix ft Rushawn - Its a beautiful day',
+    src: beautiful,
+    author: 'Trinix ft Rushawn',
+    thumbnail: trinix,
+  },
+];
